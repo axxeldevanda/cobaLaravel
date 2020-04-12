4008951ca68eddcf1b4d3926c8fcc3d403beb655
axxeldevanda <axxeldevanda@gmail.com>
2020-04-12 11:53:44 +0700

    initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layout/main')
+
+@section('title', 'About ')
+
+@section('container')
+<div class="container">
+    <div class="row">
+    <h1 class="mt-3">Hello {{$nama}}</h1>
+    </div>
+</div>
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layout/main')
+
+@section('title', 'Hello Friend ')
+
+@section('container')
+<div class="container">
+    <div class="row">
+        <h1 class="mt-3">Hello Friend</h1>
+    </div>
+</div>
+@endsection
